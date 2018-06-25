8415ac0733727064cac6ef8b5e37535f52dcea2a
Dylan <Dylan@Dylan.Dylan>
2018-06-25 02:21:42 -0400

    added watson API key

--- Proj/routes/config/config.js
+++ Proj/routes/config/config.js
@@ -1,8 +1,10 @@
 let config = {};
 config.spotify = {};
+config.watson = {};
 
 config.spotify.callback = 'http://127.0.0.1:3000/callback';
 config.spotify.client_id = "5ea30a4f5a2f46109b85e7a720e91b8a";
 config.spotify.client_secret = "b2a291bdcc46498886a5426867da9a96";
+config.watson.apikey = "HszgJb5C0f7TxfNkUgHLZM24PtbkEDIrM7euJoQg6DNS";
 
 module.exports = config;
