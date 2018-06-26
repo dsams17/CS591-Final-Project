3284b87ca46c70e0e44ee3745ae2ad226f3bf3a6
Dylan <Dylan@Dylan.Dylan>
2018-06-26 01:13:56 -0400

    slowly we go

--- Proj/frontend/src/modules/app.module.ts
+++ Proj/frontend/src/modules/app.module.ts
@@ -4,16 +4,18 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from "../components/app/app.component";
+import { UserFormComponent } from '../components/app/user-form/user-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserFormComponent,
 
 
   ],
   imports: [
-    
+
     BrowserModule,
     HttpClientModule,
   ],
